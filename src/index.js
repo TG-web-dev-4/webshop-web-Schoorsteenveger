4b3fa8866a1b6296f1dc6fe55f34f237cbcb2f6a
Janou <janoutreffers@hotmail.com>
2022-02-02 16:52:11 +0100

    practice redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+import store  from './store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1"
+  }
+  
+});
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1
+  }
+})
+console.log(store.getState());
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +28,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
