4b1847ff6608f327cd8d2ec5e74e087f3ea337ee
Janou <janoutreffers@hotmail.com>
2022-02-14 21:34:08 +0100

    add navbar

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography, Button } from '@mui/material';
 import { ShoppingCart } from '@mui/icons-material';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import { ClassNames } from '@emotion/react';
+import { Box } from '@mui/system';
 
 // import logo from '../assets/castle_logo.png';
 
 import useStyles from '../Products/Product/styles';
+import { pink } from '@mui/material/colors';
 // import useStyles from './styles';
 
 
@@ -17,28 +20,42 @@ export const NavBar = (props) => {
 
     return (
         <>
-            <AppBar position='fixed' className={classes.appBar} color="inherit">
+            <AppBar position='fixed' className={classes.appBar} color="secondary">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
-                        <img src={`../assets/castle_logo.png`} alt="Stoker Real Estate" height="25px" className={classes.image} />
-                        Stoker Real Estate
-                    </Typography>
-                    <div className={classes.grow} />
-                    <div className={classes.button} />
-                    <IconButton aria-label='Show cart items' color="inherit">
-                        <Badge badgeContent={2} color="secondary">
-                            <ShoppingCart />
-                        </Badge>
-                    </IconButton>
+                    
+                    <Box sx={{ display: "flex", justifyContent: "space-between", alignContent: "center", alignItems: "center", width: "100%", }} component="div">
+                        <Box>
+                            <IconButton>
+                                <MenuOutlinedIcon />
+                            </IconButton>
+                        </Box>
+                        <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center",}}>
+                            <Typography variant="h6" className={classes.title} color="inherit">
+                                <img src={`../assets/castle_logo.png`} alt="Stoker Real Estate" height="150vh" className={classes.image} />                               
+                            </Typography>
+                            <Typography variant='h4'>Stoker Real Estate</Typography>
+                        </Box>
+                        <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+
+                            <Typography variant="h6" sx={{ marginRight: "2em", cursor: "Pointer", color: "greenyellow" }}>Home</Typography>
+                            <Typography variant="h6" sx={{ marginRight: "2em", cursor: "Pointer", color: "greenyellow" }}>Our properties</Typography>
+                            <Typography variant="h6" sx={{ marginRight: "2em", cursor: "Pointer", color: "greenyellow" }}>About us</Typography>
+                            <Typography variant="h6" sx={{ marginRight: "2em", cursor: "Pointer", color: "greenyellow" }}>Contact</Typography>
+                        </Box>
+                    </Box>
+                    <Box>
+                        <Button variant='contained' sx={{ marginRight: "2em", cursor: "Pointer", color: "greenyellow" }}>Login</Button>
+                    </Box>
+                    <Box>
+                        <IconButton aria-label='Show cart items' color="inherit">
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </Box>
+                   
                 </Toolbar>
-                <div className="links">
-                    <Link to="/Home">Home</Link>
-                    <Link to="/Products">The Houses</Link>
-                    <Link to="/About us">About us</Link>
-                    <Link to="/Contact">Contact</Link>
-                    <Link to="/Login/Register">Login/Register</Link>
-                    {/* <Link to="/Searchbar">Searchbar</Link> */}
-                </div>
+              
             </AppBar>
         </>
 
