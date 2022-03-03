f31fae9ed3c97ff943913f04a9c7071827db1404
Janou <janoutreffers@hotmail.com>
2022-03-03 14:54:15 +0100

    add hasrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
 import Reset from "./Components/Reset";
