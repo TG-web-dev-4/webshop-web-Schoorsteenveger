5fc2167f6bbd58f9276e5b2614666906fab0b00f
Janou <janoutreffers@hotmail.com>
2022-03-03 16:10:45 +0100

    path images

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -33,7 +33,7 @@ function Header() {
         </Container>
 
         <Container minWidth='lg'>
-          <Grid item sx={{ width: '100%', minHeight: '35vh', background: 'url(./assets/home.jpg)', backgroundSize: "contain", backgroundRepeat: "no-repeat" }}
+          <Grid item sx={{ width: '100%', minHeight: '35vh', background: 'url(../assets/images/home.jpg)', backgroundSize: "contain", backgroundRepeat: "no-repeat" }}
           >
           </Grid>
         </Container>
